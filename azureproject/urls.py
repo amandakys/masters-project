d24ba5b39612a09906857d6fb8c8e9078901d45c
Andi Koh <amandakys@gmail.com>
2022-10-15 18:52:04 +0100

    remove static changes

--- azureproject/urls.py
+++ azureproject/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("account/", include("account.urls")),
     path('account/', include("django.contrib.auth.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
   

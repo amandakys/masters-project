a2034afc827cfc533bd1aa683e92a96edf8f0d40
Andi Koh <amandakys@gmail.com>
2022-10-15 17:54:53 +0100

    added static url

--- azureproject/urls.py
+++ azureproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,5 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("account/", include("account.urls")),
     path('account/', include("django.contrib.auth.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
   

80afaa65d216432ef8d18c9990bda43eabc3ca26
Andi Koh <amandakys@gmail.com>
2022-10-15 15:42:03 +0100

    updated to postgres db

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -80,25 +80,25 @@ WSGI_APPLICATION = 'azureproject.wsgi.application'
 # To use sqllite as the database engine, 
 #   uncomment the following block and comment out the Postgres section below
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 
 # Configure Postgres database for local development
 #   Set these environment variables in the .env file for this project.  
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ['DBNAME'],
-#         'HOST': os.environ['DBHOST'],
-#         'USER': os.environ['DBUSER'],
-#         'PASSWORD': os.environ['DBPASS'] 
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': os.environ['DBHOST'],
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS'] 
+    }
+}
 
 
 

10f9590a878c6f7e29f192ca12407c09d630bca4
Andi Koh <amandakys@gmail.com>
2022-10-18 14:36:10 +0100

    fixed database

--- restaurant_review/models.py
+++ restaurant_review/models.py
@@ -5,6 +5,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
 class Restaurant(models.Model):
@@ -37,3 +38,11 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     try:
+#         instance.profile.save()
+#     except ObjectDoesNotExist:
+#         Profile.objects.create(user=instance)
